df4e81dfcfb9c8d3b13226f901ad7a59bb839622
AltamashBebal <62334744+AltamashBebal@users.noreply.github.com>
2020-11-16 21:38:54 +0530

    Update settings.py

--- MyNotesManager/settings.py
+++ MyNotesManager/settings.py
@@ -128,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
     # os.path.join(BASE_DIR,'static/'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticFilesNew')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
